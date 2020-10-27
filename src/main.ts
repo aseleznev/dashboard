c715bc2bb41ee846cfaa0817dbf3b61f235eb2f0
aseleznev <aa.seleznev@gmail.com>
2020-10-27 16:29:10 +0500

    use helmet

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+  app.use(helmet());
 
   const options = new DocumentBuilder()
     .setTitle('Boss Dashboard')
@@ -14,7 +16,6 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBasicAuth({ type: 'http', scheme: 'basic' }, 'local')
     .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, 'JWT')
-    .addTag('dashboard')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);

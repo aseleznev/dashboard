650b88e16c7007d8064fb99674306b43acce92e8
aseleznev <aa.seleznev@gmail.com>
2020-10-28 15:35:02 +0500

    add roles guard

--- src/well/well.controller.ts
+++ src/well/well.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { WellService } from './well.service';
 import { Well } from './well.entity';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
+@ApiBearerAuth('JWT')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles('readWells')
 @ApiTags('well')
 @Controller('well')
 export class WellController {
--- src/well/well.module.ts
+++ src/well/well.module.ts
@@ -3,9 +3,10 @@ import { WellService } from './well.service';
 import { WellController } from './well.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Well } from './well.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Well])],
+  imports: [TypeOrmModule.forFeature([Well]), AuthModule],
   providers: [WellService],
   controllers: [WellController]
 })

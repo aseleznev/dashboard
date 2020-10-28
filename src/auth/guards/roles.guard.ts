5efc32c00312a14bf7ec4038c79c21359b7c9943
aseleznev <aa.seleznev@gmail.com>
2020-10-28 15:52:19 +0500

    move machRoles func to guard

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,18 +1,22 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { AuthService } from '../auth.service';
+import { Role } from '../../role/role.entity';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector, private readonly authService: AuthService) {}
+  constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    const roles = this.reflector.get<string[]>('roles', context.getClass());
     if (!roles) {
       return true;
     }
     const request = context.switchToHttp().getRequest();
     const user = request.user;
-    return this.authService.matchRoles(roles, user.roles);
+    return this.matchRoles(roles, user.roles);
+  }
+
+  matchRoles(roles: String[], userRoles: Role[]): boolean {
+    return userRoles.every(userRole => roles.includes(userRole.name));
   }
 }
